d6e1ab81ca909ca068dc900e5935570d58735aaf
Vasili Lysau <Vasili_Lysau@epam.com>
2020-02-20 12:15:26 +0300

    Module1: done

---
+++ Module1.1/Task1.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Task2.Library;
+
+namespace Task1.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Name was not passed through command line parameter");
+                System.Console.Write("Enter your name: ");
+                name = System.Console.ReadLine();
+            }
+            else
+            {
+                System.Console.WriteLine("Name derived from command line options");
+                name = args.FirstOrDefault();
+            }
+
+
+            System.Console.WriteLine($"{WelcomeUser.Welcome(name)}");
+            System.Console.WriteLine("Press any key to close the application");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Module1.1/Task2.Library/WelcomeUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task2.Library
+{
+    public class WelcomeUser
+    {
+        public static string Welcome(string name)
+        {
+            return $"{DateTime.Now:hh:mm:ss} Hello, {name}!";
+        }
+    }
+}
+
+
+
+
+
