354e0fa990c1b4a42521489c3bd96d65d9c2cde9
Vasili Lysau <Vasili_Lysau@epam.com>
2020-02-20 19:09:52 +0300

    Module1.2.NuGet: Done

---
+++ Module1.2.NuGet/NuGet.Package/BL/CarFileRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Text;
+using NuGet.Package.Models;
+
+namespace NuGet.Package.BL
+{
+    public static class CarFileRepository
+    {
+        public static bool WriteJson(string fileName, string content)
+        { 
+            using (var sw = new StreamWriter(fileName, false, Encoding.Default))
+            {
+                sw.WriteLine(content);
+            }
+
+            return true;
+        }
+
+        public static bool Write(string fileName, Car car)
+        {
+            var content = CarSerialization.CarToJson(car);
+            using (var sw = new StreamWriter(fileName, false, Encoding.Default))
+            {
+                sw.WriteLine(content);
+            }
+
+            return true;
+        }
+
+        public static string ReadJson(string fileName)
+        {
+            using (var sr = new StreamReader(fileName))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        public static Car Read(string fileName)
+        {
+            using (var sr = new StreamReader(fileName))
+            {
+                var json = sr.ReadToEnd();
+                return CarSerialization.JsonToCar(json);
+            }
+        }
+    }
+}
---
+++ Module1.2.NuGet/NuGet.Package/Models/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NuGet.Package.Models
+{
+    [Serializable]
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
