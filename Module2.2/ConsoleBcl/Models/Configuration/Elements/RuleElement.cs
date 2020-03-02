67676da3b541d3bbd9f622e0777cc79ea7c7569d
Vasili Lysau <Vasili_Lysau@epam.com>
2020-03-02 18:29:19 +0300

    Module2.2: Refactoring RuleElement

--- Module2.2/ConsoleBcl/Models/Configuration/Elements/RuleElement.cs
+++ Module2.2/ConsoleBcl/Models/Configuration/Elements/RuleElement.cs
@@ -4,16 +4,16 @@ namespace ConsoleBcl.Models.Configuration.Elements
 {
     class RuleElement : ConfigurationElement
     {
-        [ConfigurationProperty("folder", IsKey = true)]
+        [ConfigurationProperty("folder", IsKey = true, IsRequired = false)]
         public string Folder => (string)base["folder"];
 
-        [ConfigurationProperty("fileFilter")]
+        [ConfigurationProperty("fileFilter", IsRequired = true)]
         public string FileFilter => (string)base["fileFilter"];
 
-        [ConfigurationProperty("addNumber")]
+        [ConfigurationProperty("addNumber", IsRequired = false, DefaultValue = false)]
         public bool AddNumber => (bool)base["addNumber"];
 
-        [ConfigurationProperty("addDate")]
+        [ConfigurationProperty("addDate", IsRequired = false, DefaultValue = false)]
         public bool AddDate => (bool)base["addDate"];
     }
 }
