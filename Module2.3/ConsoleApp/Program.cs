8933c3128958bef9ec467e2ff2255e3703ef0d77
Vasili Lysau <Vasili_Lysau@epam.com>
2020-03-03 17:38:08 +0300

    Module2.3: Done

---
+++ Module2.3/ConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string exit = "exit";
+
+            Console.WriteLine("This program prints the first character of each input line");
+            Console.WriteLine($"Print '{exit}' for exit");
+
+            do
+            {
+                try
+                {
+                    var userString = Console.ReadLine();
+
+                    if (userString != null && exit.Equals(userString.ToLower()))
+                    {
+                        break;
+                    }
+
+                    var firstChar = GetFirstChar(userString);
+
+                    Console.WriteLine($"First char: '{firstChar}', string: '{userString}'");
+                }
+                catch (ArgumentNullException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            } while (true);
+        }
+
+        private static char GetFirstChar(string userString)
+        {
+            if (string.IsNullOrEmpty(userString))
+            {
+                throw new ArgumentNullException(nameof(userString), "The string cannot be empty");
+            }
+
+            if (string.IsNullOrWhiteSpace(userString))
+            {
+                throw new ArgumentException("The string must consist of at least one non-empty character", nameof(userString));
+            }
+
+            return userString[0];
+        }
+    }
+}
---
+++ Module2.3/ConsoleAppTwo/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using ParserLibrary;
+
+namespace ConsoleAppTwo
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string exit = "exit";
+
+            Console.WriteLine("This program for convert strings to numbers");
+            Console.WriteLine($"Print '{exit}' for exit");
+
+            PrintTest();
+
+            while (true)
+            {
+                Console.Write("Enter string: ");
+                var userString = Console.ReadLine();
+
+                if (userString != null && exit.Equals(userString.ToLower()))
+                {
+                    break;
+                }
+
+                Process(userString);
+            }
+        }
+
+
+        static void Process(string sourceString)
+        {
+            var parser = new IntegerParser();
+
+            Console.Write($"Source string: '{sourceString}', result: ");
+
+            try
+            {
+                var result = parser.ConvertToInt(sourceString);
+                Console.WriteLine($"{result}");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"ArgumentException: {e.Message}");
+            }
+            catch (IncorrectFormatException e)
+            {
+                Console.WriteLine($"IncorrectFormatException: {e.Message}");
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine($"OverflowException: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e.Message}");
+            }
+        }
+
+        static void PrintTest()
+        {
+            Console.WriteLine("Demo:");
+            var testList = new List<string>
+            {
+                "0",
+                "12345",
+                "-12345",
+                int.MinValue.ToString(),
+                int.MaxValue.ToString(),
+                "",
+                "   ",
+                "qwerty",
+                "12345678910",
+                "--12345",
+                "12-345",
+                "-123-45",
+                "-12345-",
+                ((long) int.MinValue - 1).ToString(),
+                ((long) int.MaxValue + 1).ToString(),
+                "9876543210"
+            };
+
+            foreach (var testString in testList)
+            {
+                Process(testString);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
+
