073479b8b57bcad5f2fc00e1584f78441fbc842a
Vasili Lysau <Vasili_Lysau@epam.com>
2020-03-11 18:00:58 +0300

    Module3: Task3 done

---
+++ Module3/Task3/SSDT/Northwind_v.1.1/dbo/Tables/CreditCards.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE dbo.CreditCards
+(
+	Id int not null IDENTITY(1,1),
+    Number NVARCHAR(20) NOT NULL,
+	ExpirationDate DATETIME NOT NULL,
+	CardHolder NVARCHAR(50) NOT NULL,
+	EmployeeId int not null,
+	constraint FK_Employee foreign key (EmployeeId) references Employees(EmployeeID),
+	constraint PK_CreditCard primary key Nonclustered (Id)
+)
